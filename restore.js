9f794ca9f5041f3e9f18b175eeab77f9f2753b09
3846masa <3846masahiro+git@gmail.com>
2015-11-04 01:06:21 +0900

    CSSでできることはCSSに変更

--- restore.js
+++ restore.js
@@ -1,22 +1,4 @@
 setTimeout(function restoreFavStar() {
-	var $heart = $('.withHeartIcon');
-	if ($heart.length >= 0) {
-		$heart.removeClass('withHeartIcon');
-	}
-
-	var $container = $('.HeartAnimationContainer');
-	if ($container.length >= 0) {
-		$container.empty();
-		$container.removeClass('HeartAnimationContainer');
-		$container.addClass('Icon Icon--favorite');
-	}
-
-	var $heartBadge = $('.Icon--heartBadge');
-	if ($heartBadge.length >= 0) {
-		$heartBadge.removeClass('Icon--heartBadge');
-		$heartBadge.addClass('Icon--favorited');
-	}
-
 	var $tooltips = $('.tooltip-inner:contains(いいね)');
 	if ($tooltips.length >= 0) {
 		$tooltips.text('お気に入りに登録');
